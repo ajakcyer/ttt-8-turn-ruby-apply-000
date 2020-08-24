37224dbb97c6549579780d242c9b507a2a05d6fa
Ajak Cyer <ajakcyer97@gmail.com>
2020-08-24 19:11:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,9 +33,6 @@ def turn(board)
   if valid_move?(board, index)
     move(board, index, token = "X")
   else
-    puts "Please enter 1-9:"
-    user_input = gets.strip
-    index = input_to_index(user_input)
-  end
+    turn
 end
 
