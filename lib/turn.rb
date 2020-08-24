da1d696ed7213f4c6b6861aab4c826f8f6b7ebe4
Ajak Cyer <ajakcyer97@gmail.com>
2020-08-24 18:44:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,9 +11,12 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+def position_taken?(board, index)
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
 
 def valid_move?(board, index)
-  
+  index.between?(0, 8) && !position_taken?(board, index)
 end
 
 
